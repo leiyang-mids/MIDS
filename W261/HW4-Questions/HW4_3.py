6b6dfcc06dd65912c261fa95ca0a6cd4d7b2556c
leiyang-mids <ynglei@gmail.com>
2016-02-06 11:48:39 -0500

    HW4.3 temp final with hadoop doesn't work on partition settings.

--- W261/HW4-Questions/HW4_3.py
+++ W261/HW4-Questions/HW4_3.py
@@ -2,32 +2,39 @@ from mrjob.job import MRJob
 from mrjob.step import MRStep
  
 class FreqVisitPage(MRJob):
-    n_freq, i = 5, 0
+    n_freq, i = 5000000, 0
         
     def mapper_count(self, dummy, line): 
+        self.increment_counter('HW4_3','page_map',1)
         # get page id
         pID = line.strip().split(',')[1]
         yield pID, 1
         
     def reducer_count(self, page, count):
-        yield page, sum(count)
+        self.increment_counter('HW4_3','page_count',1)
+        yield (sum(count), page), None
         
-    def reducer_sort(self, page, count):
-        self.increment_counter('HW4_3','sort',1)
-        if self.i < self.n_freq:
-            self.i += 1
-            yield page, sum(count)
+    def reducer_sort(self, count, _):
+        self.increment_counter('HW4_3','page_sort',1)
+        #if self.i < self.n_freq:
+        #    self.i += 1
+        #    yield page, sum(count)
+        yield count
         
     def steps(self):
-        custom_jobconf = {  #key value pairs
-            'mapred.output.key.comparator.class': 'org.apache.hadoop.mapred.lib.KeyFieldBasedComparator',
-            'mapred.text.key.comparator.options': '-k2,2nr',
-            'mapred.reduce.tasks': '1',
-            'hadoop-home': '/usr/local/Cellar/hadoop/2.7.1/libexec/etc/hadoop'
+        orig_jobconf = super(FreqVisitPage, self).jobconf()        
+        custom_jobconf = {  #key value pairs            
+            'mapreduce.job.output.key.comparator.class': 'org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator',
+            'mapreduce.partition.keycomparator.options': '-k1,1nr',
+            'mapreduce.job.maps': '1',
+            'mapreduce.job.reduces': '1',
+            'stream.map.output.field.separator': ','
         }
+        combined_jobconf = orig_jobconf
+        combined_jobconf.update(custom_jobconf)
         return [MRStep(mapper=self.mapper_count, 
-                       reducer=self.reducer_count),
-                MRStep(reducer=self.reducer_sort, jobconf=custom_jobconf)
+                       reducer=self.reducer_count)
+                ,MRStep(reducer=self.reducer_sort) #, jobconf=combined_jobconf)
                ]
     
 
