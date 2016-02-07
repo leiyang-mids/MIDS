69daba59f5680778295679ff375750cbc5888e05
leiyang-mids <ynglei@gmail.com>
2016-02-07 00:49:09 -0500

    HW4.3 sort is working with mrjob on hadoop!!!

--- W261/HW4-Questions/HW4_3.py
+++ W261/HW4-Questions/HW4_3.py
@@ -8,14 +8,14 @@ class FreqVisitPage(MRJob):
         self.increment_counter('HW4_3','page_map',1)
         # get page id
         pID = line.strip().split(',')[1]
-        yield pID, 1
+        yield pID.strip(), 1
         
     def reducer_count(self, page, count):
         self.increment_counter('HW4_3','page_count',1)
-        yield (page, sum(count)), None
+        yield page, sum(count)
         
-    def mapper_sort(self, key, value):
-        yield key, value
+    def mapper_sort(self, page, count):        
+        yield (page, count), None
         
     def reducer_sort(self, key, _):
         self.increment_counter('HW4_3','page_sort',1)
@@ -32,14 +32,14 @@ class FreqVisitPage(MRJob):
             'mapreduce.job.maps': '1',
             'mapreduce.job.reduces': '1',
             'stream.num.map.output.key.fields': '2',
-            'mapreduce.map.output.key.field.separator': ',',
-            'stream.map.output.field.separator': ','
+            'mapreduce.map.output.key.field.separator': ' '
+            #'stream.map.output.field.separator': ','
         }
         combined_jobconf = orig_jobconf
         combined_jobconf.update(custom_jobconf)
-        # TODO: doesn work on hadoop with jobconf set, need to check
+        
         return [MRStep(mapper=self.mapper_count, reducer=self.reducer_count)
-                ,MRStep(mapper=self.mapper_sort, reducer=self.reducer_sort, jobconf=combined_jobconf)
+                ,MRStep(mapper=self.mapper_sort, reducer=self.reducer_sort, jobconf=custom_jobconf)
                ]
     
 
