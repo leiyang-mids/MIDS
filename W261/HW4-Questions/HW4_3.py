c01abf812e2936c983b86a0c7b53f6196600e6ce
leiyang-mids <ynglei@gmail.com>
2016-02-06 23:55:10 -0500

    HW4.3 sorting works, but on the wrong column of page ID - need to move to count column.

--- W261/HW4-Questions/HW4_3.py
+++ W261/HW4-Questions/HW4_3.py
@@ -12,29 +12,34 @@ class FreqVisitPage(MRJob):
         
     def reducer_count(self, page, count):
         self.increment_counter('HW4_3','page_count',1)
-        yield (sum(count), page), None
+        yield (page, sum(count)), None
         
-    def reducer_sort(self, count, _):
+    def mapper_sort(self, key, value):
+        yield key, value
+        
+    def reducer_sort(self, key, _):
         self.increment_counter('HW4_3','page_sort',1)
         #if self.i < self.n_freq:
         #    self.i += 1
         #    yield page, sum(count)
-        yield count
+        yield key
         
     def steps(self):
         orig_jobconf = super(FreqVisitPage, self).jobconf()        
         custom_jobconf = {  #key value pairs            
             'mapreduce.job.output.key.comparator.class': 'org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator',
-            'mapreduce.partition.keycomparator.options': '-k1,1nr',
+            'mapreduce.partition.keycomparator.options': '-k2,2nr',
             'mapreduce.job.maps': '1',
             'mapreduce.job.reduces': '1',
+            'stream.num.map.output.key.fields': '2',
+            'mapreduce.map.output.key.field.separator': ',',
             'stream.map.output.field.separator': ','
         }
         combined_jobconf = orig_jobconf
         combined_jobconf.update(custom_jobconf)
         # TODO: doesn work on hadoop with jobconf set, need to check
         return [MRStep(mapper=self.mapper_count, reducer=self.reducer_count)
-                ,MRStep(reducer=self.reducer_sort) #, jobconf=combined_jobconf)
+                ,MRStep(mapper=self.mapper_sort, reducer=self.reducer_sort, jobconf=combined_jobconf)
                ]
     
 
