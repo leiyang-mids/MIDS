697ded1d6c3cc6bb878bf2437eb024e6198cde52
leiyang-mids <ynglei@gmail.com>
2016-02-06 23:37:14 -0500

    HW4.4 - done count & convert - need to sort - although all is 1.

--- W261/HW4-Questions/HW4_4.py
+++ W261/HW4-Questions/HW4_4.py
@@ -3,16 +3,47 @@ from mrjob.step import MRStep
  
 class FreqVisitor(MRJob):
     
-    def mapper_vistor(self, _, line):
-        temp = line.strip().split(',')
-        # emit page ID and visitor ID as key
-        yield 'C_%s_V_%s' %(temp[4].strip('"'), temp[1]), 1
+    # visitor ID and url
+    url = visitorID = None
         
-    def reducer_visitor(self, page, count):
-        yield page, sum(count)
+    # mapper
+    def convert_mapper(self, _, line):        
+        # time of mapper being called
+        self.increment_counter('HW4_2', 'lines', 1)
+        # only emit lines start with C and V
+        line = line.strip()
+        if line[0] not in ['C', 'V', 'A']:
+            return
+        temp = line.split(',')
+        # process A, C, and V lines
+        if line[0] == 'C':            
+            # get the latest visitor ID
+            self.visitorID = temp[2]  
+        elif line[0] == 'A':
+            # emit V_pageID_*_url as key, dummy 1 as value
+            yield 'V_%s_*_%s' %(temp[1], temp[4].strip('"')), 1
+        else:
+            # emit V_pageID_C_visitorID as key, 1 as value
+            yield 'V_%s_C_%s' %(temp[1], self.visitorID), 1
+            
+    
+    # reducer to get count for each visitor on each page
+    def count_reducer(self, key, value):
         
+        temp = key.strip().split('_')
+        # save webpage url for the following visisting records
+        if temp[2] == '*':
+            self.url = temp[3]
+        else:
+            yield key+'_'+self.url, sum(value)
+        
+    # MapReduce steps
     def steps(self):
-        return [MRStep(mapper=self.mapper_vistor, reducer=self.reducer_visitor)]
+        custom_jobconf = {  #key value pairs                        
+            'mapreduce.job.maps': '1',
+            'mapreduce.job.reduces': '1'            
+        }
+        return [MRStep(mapper=self.convert_mapper, reducer=self.count_reducer, jobconf=custom_jobconf)]
 
     
 if __name__ == '__main__':
