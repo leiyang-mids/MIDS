d314e6b5039a73d34abfe4fdeb15383f3469c934
leiyang-mids <ynglei@gmail.com>
2016-02-08 16:53:20 -0500

    HW4.5 working version.

--- W261/HW4-Questions/HW4_5_Kmeans_driver.py
+++ W261/HW4-Questions/HW4_5_Kmeans_driver.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
+
 #%reload_ext autoreload
 #%autoreload 2
 import numpy as np
 from HW4_5_Kmeans import MRKmeans, stop_criterion
-import os
 
 # create MrJob: specify input, available file, hadoop home
 mr_job = MRKmeans(args=['HW4_5_train_data.csv', '--file', 'Centroids.txt',
@@ -17,24 +17,21 @@ i = 1
 while(1):
     # save previous centoids to check convergency
     centroid_points_old = centroid_points[:]
+    np.savetxt('c_old', centroid_points_old, delimiter=',')
     print "Iteration "+str(i)+" ..."
     with mr_job.make_runner() as runner: 
         runner.run()
         # stream_output: get access of the output         
         for line in runner.stream_output():
-            key,value =  mr_job.parse_output_line(line)
-            print key, value
+            key,value = mr_job.parse_output_line(line)            
             centroid_points[key] = value        
             
-    print "\n"
     i += 1
     # put the latest centroids in the file for next iteration    
-    os.remove('Centroids.txt')  
-    with open('Centroids.txt', 'w+') as f:
-        f.writelines(','.join(str(j) for j in i) + '\n' for i in centroid_points)
-    
+    np.savetxt('Centroids.txt', centroid_points, delimiter = ",")
+        
     # check stopping criterion
-    if(stop_criterion(centroid_points_old,centroid_points,0.001)):
+    if stop_criterion(np.genfromtxt('c_old', delimiter=','), centroid_points, 0.001):
         break
         
-print '\nK-means training completed after %d iterations!' %i
+print '\nK-means training completed after %d iterations!' %(i-1)
--- W261/HW4-Questions/HW4_5_get_composition.py
+++ W261/HW4-Questions/HW4_5_get_composition.py
@@ -6,7 +6,7 @@ from HW4_5_Kmeans import MinDist
 
 # load the centroids
 centroids = np.genfromtxt('Centroids.txt', delimiter=',') 
-compo = [[0,0,0,0] for in len(centroids)]
+compo = [[0,0,0,0] for i in range(len(centroids))]
 
 # load the raw data
 raw_data = np.genfromtxt('topUsers_Apr-Jul_2014_1000-words.txt', delimiter=',')
@@ -20,5 +20,6 @@ for r in raw_data:
 #with open('Composition.txt', 'w+') as f:
 #    f.writelines(','.join(str(j) for j in i) + '\n' for i in centroid_points)
 for i in range(len(compo)):
+    print '\n'
     for j in range(4):
-        print 'Centroid %d - code %d: %d (%.2f%%)' %(i, j, compo[i][j], 100.0*compo[i][j]/1000)
+        print 'Centroid %d - code %d: %d (%.2f%%)' %(i, j, compo[i][j], 100.0*compo[i][j]/sum(compo[i]))
