43ae486b4c39e17fa39e761789ff51146d226e21
leiyang-mids <ynglei@gmail.com>
2016-02-13 00:02:50 -0500

    job 2 mapper!

--- W261/HW5-Questions/HW5_4_Jaccard.py
+++ W261/HW5-Questions/HW5_4_Jaccard.py
@@ -25,13 +25,16 @@ class Jaccard(MRJob):
     # job 1 reducer
     def j1_reducer(self, key, count):
         term, doc = key[0], key[1]   
-        self.count = sum(count)
+        #self.count = sum(count)
+        
         if self.current_term == term:
+            # accumulate postings
             self.current_strip.append(doc)
         else:
+            # yield previous term and stripe
             if self.current_term:
                 yield self.current_term, self.current_strip
-            # reset
+            # reset new term
             self.current_term = term
             self.current_strip = [doc]
         
@@ -40,10 +43,23 @@ class Jaccard(MRJob):
         if self.current_term:
             yield self.current_term, self.current_strip
             
+    # job 2 mapper - emit pair-wise similarity from strips
+    def j2_mapper(self, term, postings):
+        # get all postings from generator
+        posts = [p for p in postings]
+        size = len(posts)
+        # emit dummy for order inversion
+        for p in posts:
+            yield ('*', p), 1
+        # emit pairs (here assuming job 1 partitioner did secondary sort)
+        for p1, p2 in [[posts[i], posts[j]] for i in range(size) for j in range(i+1, size)]:
+            yield (p1, p2), 1
+            
     # MapReduce steps
     def steps(self):
         return [MRStep(mapper=self.j1_mapper_test, reducer_init=self.j1_reducer_init,
-                       reducer=self.j1_reducer, reducer_final=self.j1_reducer_final)
+                       reducer=self.j1_reducer, reducer_final=self.j1_reducer_final),
+                MRStep(mapper=self.j2_mapper)
                ]
 
 if __name__ == '__main__':
