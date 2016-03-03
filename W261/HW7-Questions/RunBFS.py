f362eb9b768e7eb5625c2cf4146454ea3e357480
Lei Yang <ynglei@gmail.com>
2016-03-03 18:02:37 -0500

    weighted BFS

--- W261/HW7-Questions/RunBFS.py
+++ W261/HW7-Questions/RunBFS.py
@@ -6,7 +6,7 @@ import sys, getopt
 
 # parse parameter
 if __name__ == "__main__":
-    
+
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hg:s:d:m:")
     except getopt.GetoptError:
@@ -27,7 +27,7 @@ if __name__ == "__main__":
             destination = arg
         elif opt == '-m':
             mode = arg
-        
+
 
 # creat BFS job
 init_job = ShortestPathIter(args=[graph, '--source', source, '-r', mode])
@@ -36,60 +36,22 @@ iter_job = ShortestPathIter(args=['graph', '--source', source, '-r', mode])
 # run initialization job
 with init_job.make_runner() as runner:
     runner.run()
-    dist_old = {}
-    # save our graph file for iteration
-    with open('graph', 'w') as f:
-        for line in runner.stream_output():
-            # value is nid and node object
-            nid, node = init_job.parse_output_line(line)
-            # record distance for each node
-            dist_old[nid] = node['dist']
-            # write graph file 
-            f.write('%s\t%s\n' %(nid, node))
 
 # run BFS iteratively
 i = 1
-while(1):    
+while(1):
     print 'iteration %s' %i
-    dist = {}
-    with iter_job.make_runner() as runner: 
+    stop = False
+    with iter_job.make_runner() as runner:
         runner.run()
-        # stream_output: get access of the output    
-        with open('graph', 'w') as f:
-            for line in runner.stream_output():
-                # value is nid and node object
-                nid, node = iter_job.parse_output_line(line)
-                dist[nid] = node['dist']
-                f.write('%s\t%s\n' %(nid, str(node)))
-            
-    # check if distance for each node changes
-    stop = True
-    for n in dist:
-        if dist_old[n] != dist[n]:
-            stop = False
-            break  
-    
+        # check if no distance has changed
+        stop = runner.counters() == None # ['weighted']['dist_changed'] == 0
+
+    # retrieve path if stop critierion is satisfied
     if stop:
         break
-    
-    # save dist for next iteration comparison
-    dist_old = dist
-    i += 1
-        
-print "Traversing completes!\n"
 
-# show path between source and destination
-with open('graph', 'r') as f:
-    line = f.readline()
-    while (line):
-        nid, node = line.split('\t')
-        if nid == destination:
-            cmd = 'node = %s' %node
-            exec cmd
-            if node['path']:
-                print 'shortest distance between %s and %s: %s' %(source, destination, node['dist'])
-                print 'path: %s' %' -> '.join(node['path']+[destination])
-            else:
-                print '%s is a dangling node, cannot traverse from it!' %source
-            break
-        line = f.readline()
+    # save dist for next iteration comparison
+    i += 1
+
+print "Traversing completes!\n"
--- W261/HW7-Questions/ShortestPathIter.py
+++ W261/HW7-Questions/ShortestPathIter.py
@@ -4,55 +4,57 @@ from mrjob.step import MRStep
 
 class ShortestPathIter(MRJob):
     DEFAULT_PROTOCOL = 'json'
-    
+
     def __init__(self, *args, **kwargs):
         super(ShortestPathIter, self).__init__(*args, **kwargs)
-        
-                                                 
+
+
     def configure_options(self):
         super(ShortestPathIter, self).configure_options()
         self.add_passthrough_option(
             '--source', dest='source', default='1', type='string',
-            help='source: source node (default 1)')        
+            help='source: source node (default 1)')
 
     def mapper(self, _, line):
         nid, dic = line.strip().split('\t', 1)
         cmd = 'node = %s' %dic
-        exec cmd        
-        # if the node structure is incomplete (first pass), add them                
-        if 'dist' not in node:            
-            node = {'adj':node, 'path':[]}            
-            node['dist'] = 0 if self.options.source==nid else -1            
+        exec cmd
+        # if the node structure is incomplete (first pass), add them
+        if 'dist' not in node:
+            node = {'adj':node, 'path':[]}
+            node['dist'] = 0 if self.options.source==nid else -1
         # emit node
-        yield nid, node        
+        yield nid, node
         # emit distances to reachable nodes
         if node['dist'] >= 0:
-            for m in node['adj']:                
-                ##### for regular weighted dataset
-                #yield m, {'dd':node['adj'][m] + node['dist'], 'pp':node['path']+[nid]}
-                ##### for wikipedia dataset, no weight
-                yield m, {'dd':1 + node['dist'], 'pp':node['path']+[nid]}
-                
+            for m in node['adj']:
+                yield m, {'dd':node['adj'][m] + node['dist'], 'pp':node['path']+[nid]}
+
     def reducer(self, nid, value):
         dmin = float('inf')
         path = node = None
         # loop through all arrivals
-        for v in value:            
+        for v in value:
             if 'dist' in v:
                 node = v
             elif v['dd'] < dmin:
                 dmin = v['dd']
                 path = v['pp']
-        # handle dangling node
+
+        # handle dangling node, we only care if it's destination
         if not node:
-            node = {'adj':{}, 'dist':dmin, 'path':path}
-        # update distance and path
-        if (node['dist'] == -1 and path) or dmin < node['dist']:
-            node['dist'] = dmin
-            node['path'] = path
+            if nid == self.options.destination:
+                node = {'adj':{}, 'dist':dmin, 'path':path}
+                self.increment_counter('weighted', 'dist_changed', 1)
+            else:
+                return
+        elif (node['dist'] == -1 and path) or dmin < node['dist']:
+            node['dist'], node['path'] = dmin, path
+            self.increment_counter('weighted', 'dist_changed', 1)
+
         # emit for next iteration
         yield nid, node
-        
+
     def steps(self):
         jc = {
             'mapreduce.job.maps': '10',
