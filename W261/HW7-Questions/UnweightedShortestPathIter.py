9258e54958d610c5cb9e70001acf7dcf384dc93d
leiyang-mids <ynglei@gmail.com>
2016-03-03 00:42:03 -0500

    EMR wiki testing.

--- W261/HW7-Questions/UnweightedShortestPathIter.py
+++ W261/HW7-Questions/UnweightedShortestPathIter.py
@@ -4,41 +4,41 @@ from mrjob.step import MRStep
 
 class UnweightedShortestPathIter(MRJob):
     DEFAULT_PROTOCOL = 'json'
-    
+
     def __init__(self, *args, **kwargs):
         super(UnweightedShortestPathIter, self).__init__(*args, **kwargs)
-        
-                                                 
+
+
     def configure_options(self):
         super(UnweightedShortestPathIter, self).configure_options()
         self.add_passthrough_option(
             '--source', dest='source', default='1', type='string',
-            help='source: source node (default 1)') 
+            help='source: source node (default 1)')
         self.add_passthrough_option(
             '--destination', dest='destination', default='1', type='string',
-            help='destination: destination node (default 1)') 
+            help='destination: destination node (default 1)')
 
     def mapper(self, _, line):
         nid, dic = line.strip().split('\t', 1)
         nid = nid.strip('"')
         cmd = 'node = %s' %dic
-        exec cmd        
-        # if the node structure is incomplete (first pass), add them                
-        if 'dist' not in node:            
-            node = {'adj':node.keys(), 'path':[]}            
-            node['dist'] = 0 if self.options.source==nid else -1            
+        exec cmd
+        # if the node structure is incomplete (first pass), add them
+        if 'dist' not in node:
+            node = {'adj':node.keys(), 'path':[]}
+            node['dist'] = 0 if self.options.source==nid else -1
         # emit node
-        yield nid, node        
+        yield nid, node
         # emit distances to reachable nodes
         if node['dist'] >= 0:
-            for m in node['adj']:                     
+            for m in node['adj']:
                 yield m, {'dd':(1+node['dist']), 'pp':(node['path']+[nid])}
-                
+
     def reducer(self, nid, value):
         dmin = float('inf')
         path = node = None
         # loop through all arrivals
-        for v in value:            
+        for v in value:
             if 'dist' in v:
                 node = v
             elif v['dd'] < dmin:
@@ -55,15 +55,15 @@ class UnweightedShortestPathIter(MRJob):
             node['path'] = path
         # emit for next iteration
         yield nid, node
-        
+
     def steps(self):
         jc = {
-            'mapreduce.job.maps': '1',
-            'mapreduce.job.reduces': '1',
+            'mapreduce.job.maps': '16',
+            'mapreduce.job.reduces': '16',
         }
         return [MRStep(mapper=self.mapper
                        , combiner=self.reducer
-                       , reducer=self.reducer                       
+                       , reducer=self.reducer
                        , jobconf = jc
                       )
                ]
