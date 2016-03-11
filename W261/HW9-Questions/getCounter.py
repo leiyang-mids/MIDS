b4857f929260a43e0ad7f83937c5c22faf6034ae
Lei Yang <ynglei@gmail.com>
2016-03-11 11:01:06 -0500

    clean up

---
+++ W261/HW9-Questions/getCounter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import requests
+def getCounter(groupName, counterName, host = 'localhost'):
+    # get job list       
+    getJobs = 'http://%s:19888/ws/v1/history/mapreduce/jobs' %host
+    jobs = requests.get(getJobs).json()['jobs']['job'] 
+    # get counters
+    ts = max([job['finishTime'] for job in jobs])
+    id = [job['id'] for job in jobs if job['finishTime'] == ts][0]
+    getCounters = 'http://%s:19888/ws/v1/history/mapreduce/jobs/%s/counters' %(host, id)
+    counterGroups = requests.get(getCounters).json()['jobCounters']['counterGroup']
+    # loop through to counters to return value
+    counters = [g['counter'] for g in counterGroups if g['counterGroupName']==groupName]
+    totalValues = [c['totalCounterValue'] for c in counters if c['name']==counterName]
+    return totalValues[0] if len(totalValues)==1 else None
