eb6fa111ab342ce5dd5cafa02e845b8198c17d01
Lei Yang <ynglei@gmail.com>
2016-07-22 14:11:15 -0400

    training main process

--- W210/ACA_LETOR/offline/main.py
+++ W210/ACA_LETOR/offline/main.py
@@ -2,53 +2,53 @@ from datetime import datetime, timedelta
 from s3_helpers import *
 from get_rank_for_state_plan import *
 from get_click_data import *
-import traceback, pickle
+import traceback, pickle, time
 import numpy as np
 
 from simulate_clicks import *
 
 def main():
 	# execute cyclically
-	next_run = datetime.now()
-	interval = 5
+	next_run, hour, minute = datetime.now(), 5, 10
+	global s3clnt, log = s3_helper(), logger('training')
+
 	while True:
 		try:
 			if datetime.now() < next_run:
+				time.sleep((next_run-datetime.now()).seconds)
 				continue
-			next_run += timedelta(hours=interval)
 			# main procedure starts here
 			print 'get query and click data '
 			click_data = get_click_data()
 			print 'group sessions by state'
-			states = np.unique(click_data['state'])
-			print 'characterize queries for each state'
-
-			print 'run letor training for each state'
-			plans, q_cluster, clicks = simulate_clicks(8)
-			letor_rank = get_rank_for_state_plan(q_cluster, clicks)
-
-			print 'save result on s3, for ES indexing'
-			save_name = 'training/%s_%d.pickle' %('UT', len(letor_rank))
-
-			with open(save_name, 'w') as f:
-				pickle.dump([plans, letor_rank], f)
-
-			# print '%s: feature matrix saved as %s' %(logTime(), saveName)
-			s3clnt = s3_helper()
-            s3clnt.upload(save_name)
-            s3clnt.set_public(save_name)
-			print 'next run time is %s' %str(next_run)
-
+			for state in np.unique(click_data['state']):
+				s_rows = click_data[click_data['state']==state]
+				print 'characterize queries for each state'
+				q_cluster, q_characterizer, centroids = query_characterizer(s_rows)
+				print 'run letor training for each state'
+				# plans, q_cluster, clicks = simulate_clicks(8)
+				letor_rank = get_rank_for_state_plan(q_cluster, np.array([[r['ranks'],r['clicks']] for r in s_rows]))
+				print 'save result on s3, for ES indexing'
+				save_name = 'training/%s_%d.pickle' %(state, len(letor_rank))
+				with open(save_name, 'w') as f:
+					pickle.dump([plans, letor_rank], f)
+				# print '%s: feature matrix saved as %s' %(logTime(), saveName)
+				s3clnt = s3_helper()
+	            s3clnt.upload(save_name)
+	            s3clnt.set_public(save_name)
+			# training completed, get next run time
+			next_run = datetime.now() + timedelta(hours=hour)
+			print 'training has completed, next run time is %s' %str(next_run)
 		except KeyboardInterrupt:
 			break
 			# sys.exit('User termination')
 		except Exception as ex:
-			traceback.print_exc()
-			print 'Execution has encountered an error, restart in 10 minutes ...'
-			next_run = datetime.now() + timedelta(minutes=10)
+			traceback.print_exc(file=log.log_handler())
+			print 'training has encountered an error, restart in %s minutes ...' %minute
+			next_run = datetime.now() + timedelta(minutes=minute)
 
 		# upload log file to S3
-
+		s3clnt.upload2(log.log_name(), 'log/'+log.log_name())
 
 if __name__ == "__main__":
 	main()
